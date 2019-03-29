7f09c0698c147de29803a463b4bc7a71620c2668
Isaac Smith <isaac.smitty@gmail.com>
2019-03-29 21:36:23 +0000

    basic user sign-up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'teams#index'
+ root to: 'teams#index'
 
-  resources :teams
+ resource :teams
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_213955) do
+ActiveRecord::Schema.define(version: 2019_03_29_195807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2019_03_28_213955) do
     t.decimal "faceOffWinPercentage", precision: 10, scale: 2
     t.decimal "shootingPctg", precision: 10, scale: 2
     t.decimal "savePctg", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
