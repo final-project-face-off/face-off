18e065a089a3bbd4ff941952512db54394e3bd53
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-04-02 12:32:31 -0400

    changed teams route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
  root to: 'teams#index'
 
-  namespace :api do
-    namespace :v1 do
-      resources :teams, only: [:index]
-    end
-  end
+  # THIS DOESN'T WORK
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :teams, only: [:index, :show]
+  #   end
+  # end
+
+  resources :teams, only: [:index, :show]
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
