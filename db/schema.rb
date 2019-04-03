0b006c88966814b9062fb02e079c2a2dbe1da085
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-04-03 17:44:27 -0400

    Added team_id & season to team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_210342) do
+ActiveRecord::Schema.define(version: 2019_04_03_213303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_210342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "season"
+    t.integer "team_id"
   end
 
   create_table "users", force: :cascade do |t|
