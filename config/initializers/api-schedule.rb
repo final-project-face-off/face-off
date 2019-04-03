9b8dce1f83a7e2526e7a60fad123fc1cd9af7778
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-04-03 11:51:00 -0400

    fixed game schedule

--- config/initializers/api-schedule.rb
+++ config/initializers/api-schedule.rb
@@ -4,11 +4,17 @@ require 'rails/configuration'
 Game.delete_all
 ActiveRecord::Base.connection.reset_pk_sequence!(:game)
 
-#NHL API endpoint
-  nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule"
+#TODAY'S GAMES
+
+#today's date
+date = Time.now
+today = date.strftime("%Y-%m-%d")
+
+#NHL API endpoint => TODAY
+nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{today}"
 
 #RestClient get request to the NHL endpoint parsed into a JS object
-  data = JSON.parse( RestClient.get("#{nhl_url}") )
+data = JSON.parse( RestClient.get("#{nhl_url}") )
 
 #Iterating through each NHL team
   data["dates"][0]["games"].each do |game, index|
@@ -30,15 +36,18 @@ ActiveRecord::Base.connection.reset_pk_sequence!(:game)
     end
   end
 
+
+#YESTERDAY'S GAMES
+
 #Calculate yesterday's date
 date = Time.now - 86400
 yesterday = date.strftime("%Y-%m-%d")
 
 #NHL API endpoint for yesterdays games
-nhl_results_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{yesterday}"
+nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{yesterday}"
 
 #RestClient get request to the NHL endpoint parsed into a JS object
-  data = JSON.parse( RestClient.get("#{nhl_results_url}") )
+data = JSON.parse( RestClient.get("#{nhl_url}") )
 
 #Iterating through each NHL team
   data["dates"][0]["games"].each do |game, index|
@@ -55,6 +64,39 @@ nhl_results_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{yesterday
     #Saving games
     if game.save
       puts "saved yesterday's game"
+    else
+      puts "not saved"
+    end
+  end
+
+
+#TOMORROW'S GAMES
+
+#Calculate tomorrow's date
+date = Time.now + 86400
+tomorrow = date.strftime("%Y-%m-%d")
+
+#NHL API endpoint for yesterdays games
+nhl_url = "https://statsapi.web.nhl.com/api/v1/schedule?date=#{tomorrow}"
+
+#RestClient get request to the NHL endpoint parsed into a JS object
+data = JSON.parse( RestClient.get("#{nhl_url}") )
+
+#Iterating through each NHL team
+  data["dates"][0]["games"].each do |game, index|
+    #Creating a new game in my database and assigning it's properties
+    game = Game.new do |key|
+      key.gameDate = game["gameDate"]
+      key.awayID = game["teams"]["away"]["team"]["id"]
+      key.awayScore = game["teams"]["away"]["score"]
+      key.awayName = game["teams"]["away"]["team"]["name"]
+      key.homeID = game["teams"]["home"]["team"]["id"]
+      key.homeScore = game["teams"]["home"]["score"]
+      key.homeName = game["teams"]["home"]["team"]["name"]
+    end
+    #Saving games
+    if game.save
+      puts "saved tomorrow's game"
     else
       puts "not saved"
     end
