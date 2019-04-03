66c7bba45d3765d050ed871f3b3d8a15be0ad13e
Isaac Smith <isaac.smitty@gmail.com>
2019-04-03 21:27:28 +0000

    working with charts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+
+# Review.create!(
+#   user_id: 1,
+#   product_id: 11,
+#   description: 'This is Great!',
+# >>>>>>> feature/product-ratings
+#   rating: 5
+#   )
+# Team.create!(
+#     id: 44,
+#     name: 'NHL',
+#     gamesPlayed: 0,
+#     wins: 0,
+#     losses: 0,
+#     ot: 0,
+#     pts: 0,
+#     ptPctg: 0,
+#     goalsPerGame: 0,
+#     goalsAgainstPerGame: 0,
+#     evGGARatio: 0,
+#     powerPlayPercentage: 0,
+#     powerPlayGoals: 0,
+#     powerPlayGoalsAgainst: 0,
+#     powerPlayOpportunities: 0,
+#     penaltyKillPercentage: 0,
+#     shotsPerGame: 0,
+#     shotsAllowed: 0,
+#     winScoreFirst: 0,
+#     winOppScoreFirst: 0,
+#     winLeadFirstPer: 0,
+#     winLeadSecondPer: 0,
+#     winOutshootOpp: 0,
+#     winOutshotByOpp: 0,
+#     faceOffsTaken: 0,
+#     faceOffsWon: 0,
+#     faceOffsLost: 0, 
+#     faceOffWinPercentage: 0,
+#     shootingPctg: 0,
+#     savePctg: 0
+#     )
